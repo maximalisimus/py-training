c1ec4a0a07b975c00ce6e14df8fe29eb94545f9d
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-09-12 11:17:22 +0300

    Update

--- Python_Training/genindex-develop/main3.py
+++ Python_Training/genindex-develop/main3.py
@@ -24,28 +24,33 @@ class Arguments:
 				f"\tBGColor: {bgcolor},\n" + \
 				f"\tEvenOdd: {evenodd},\n" + \
 				f"\tOddEven: {oddeven},\n" + \
-				f"\tSkip Dirs: {dirs},\n" + \
-				f"\tSkip files: {files},\n" + \
+				f"\tSkip Dirs: {skip_dirs},\n" + \
+				f"\tSkip Files: {skip_files},\n" + \
 				f"\tSave configs: {save},\n" + \
 				f"\tReset configs: {reset}"
 
 def createParser():
-	parser = argparse.ArgumentParser(prog=progname,description='GenIndex - create apindex file')
+	parser = argparse.ArgumentParser(prog=progname,description='GenIndex - create apindex file.')
 	group1 = parser.add_argument_group('Default', 'Default paramters.')
-	group1.add_argument('-v', '--version', action='version', version=f'{progname} {__version__}',  help='Version.')
-	group1.add_argument("-fo", '--fonts', dest="fonts", metavar='FONTS', type=str, default='sans-serif', help='Html page font.')
+	parser.add_argument('-v', '--version', action='version', version=f'{progname} {__version__}',  help='Version.')
+	group1.add_argument("-f", '--fonts', dest="fonts", metavar='FONTS', type=str, default='sans-serif', help='Html page font.')
 	group1.add_argument("-b", '--bgcolor', dest="bgcolor", metavar='BGCOLOR', type=str, default='white', help='Background color Html page.')
 	group1.add_argument("-e", '--evenodd', dest="evenodd", metavar='EVENODD', type=str, default='white', help='Color of odd lines of Html page.')
 	group1.add_argument("-o", '--oddeven', dest="oddeven", metavar='ODDEVEN', type=str, default='#C9E4F6', help='Color of even lines of the Html page.')
 	
 	group2 = parser.add_argument_group('Skip', 'SKIP paramters.')
-	group2.add_argument("-d", '--dirs', dest="dirs", metavar='DIRS', type=str, default='git,.git', help='Skip Directories.')
-	group2.add_argument("-fi", '--files', dest="files", metavar='FILES', type=str, default='', help='Skip Files.')
+	group2.add_argument("-sd", '--skip_dirs', dest="skip_dirs", metavar='SKIP_DRIS', type=str, default='git,.git', help='Skip Directories.')
+	group2.add_argument("-sf", '--skip_files', dest="skip_files", metavar='SKIP_FILES', type=str, default='', help='Skip Files.')
 	
 	group3 = parser.add_argument_group('Save', 'Save paramters.')
 	group3.add_argument('-s', '--save', action='store_true', default=False, help='Save configs.')
-	group3.add_argument('-r', '--reset', action='store_true', default=False, help='Reset settings to Default settings.')
-	return parser, group1, group2
+	group3.add_argument('-r', '--reset', action='store_true', default=False, help='Reset settings to Default config.')
+	
+	group4 = parser.add_argument_group('Actions', 'Action paramters.')
+	group4.add_argument("-d", '--dirs', dest="dirs", metavar='DIRS', type=str, default='./', help='Select the folder.')
+	group4.add_argument('-g', '--generate', action='store_true', default=False, help='Generate Generate new name for icon file.')
+	group4.add_argument('-w', '--write', action='store_true', default=False, help='Perform recording index.html files to the specified directory.')
+	return parser, group1, group2, group3, group4
 
 def DefaultConfig() -> dict:
 	return {
@@ -78,15 +83,15 @@ def main():
 	config = configparser.ConfigParser()
 	if config.sections() == []:
 		config.read(config_file)
-	parser, gr1, gr2 = createParser()
+	parser, gr1, gr2, gr3, gr4 = createParser()
 	args = Arguments()
 	parser.parse_args(namespace=Arguments)
 	config['DEFAULT']['fonts'] = args.fonts
 	config['DEFAULT']['bgcolor'] = args.bgcolor
 	config['DEFAULT']['evenodd'] = args.evenodd
 	config['DEFAULT']['oddeven'] = args.oddeven
-	config['SKIP']['dirs'] = args.dirs
-	config['SKIP']['files'] = args.files
+	config['SKIP']['dirs'] = args.skip_files
+	config['SKIP']['files'] = args.skip_files
 	if args.save:
 		WriteConfig(config)
 	if args.reset:
