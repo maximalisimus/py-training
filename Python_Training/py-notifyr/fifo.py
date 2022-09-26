49b74a05a27291d8ddd35111f85e54be02b2b20d
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-09-26 19:08:30 +0300

    Develop-start

---
+++ Python_Training/py-notifyr/fifo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import pathlib
+from os import mkfifo
+
+def main():
+	# pathlib.Path.home()
+	FIFO = pathlib.Path(sys.argv[0]).parent.joinpath('pipe1').resolve()
+	if not FIFO.exists:
+		mkfifo(FIFO)
+	print("Opening FIFO...")
+	with open(FIFO) as fifo:
+		print("FIFO opened")
+		while True:
+			data = fifo.read()
+			if len(data) == 0:
+				print("Writer closed")
+				break
+			print(f"{data}")
+			#print('Read: "{0}"'.format(data))
+
+if __name__ == '__main__':
+	main()
