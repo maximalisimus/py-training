da698d5a0950de3d46c1b292b374d8502ac43918
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-10-09 23:26:47 +0300

    Update

--- Python_Training/py-notifyr/get-volume.sh
+++ Python_Training/py-notifyr/get-volume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 volumes=$(pactl get-sink-volume @DEFAULT_SINK@ | sed '2,$d' | awk '{print $5,$12}')
-./window.py "Volume is: ${volumes[*]}"
+echo "${volumes[*]}"
 
 exit 0
--- Python_Training/py-notifyr/window.py
+++ Python_Training/py-notifyr/window.py
@@ -640,8 +640,7 @@ class Files:
 		return real_x, real_y
 
 def main():
-	param = sys.argv[1]
-	args = Arguments(icon='info.png', scale='3,3', Title='Audio Volume', Message=param, OnTime=5000,
+	args = Arguments(icon='test1.png', scale='3,3', Title='Apps!', Message='Mesages to text output information!', OnTime=5000,
 					PosX=PositionX.Right.value, PosY = PositionY.Top.value, isTimer = True, Topmost = False
 					)
 	args.BG = '#303030'
