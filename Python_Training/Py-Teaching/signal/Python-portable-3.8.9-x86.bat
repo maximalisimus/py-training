8a1b7457f28368dff5bca7e50d3a198c488b78cc
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-10-14 11:35:09 +0300

    Update

--- Python_Training/Py-Teaching/signal/Python-portable-3.8.9-x86.bat
+++
@@ -1,8 +0,0 @@
-@cd/d "%~dp0"
-@echo off
-SET PATH=C:\WSL-Win-7\python-portable\Portable-Python-3.8.9-x86\;%PATH%
-SET PATH=C:\WSL-Win-7\python-portable\Portable-Python-3.8.9-x86\Scripts\;%PATH%
-SET PATH=C:\WSL-Win-7\;%PATH%
-SET WORKON_HOME=D:\Temp\env-x86\
-DOSKEY clear=cls
-cmd.exe
