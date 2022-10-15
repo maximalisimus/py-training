001e67145e95f17bd053f78fccd5ad1254d37793
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-10-15 17:29:12 +0300

    Update

---
+++ Python_Training/version.py
@@ -0,0 +1,17 @@
+
+__all__ = ['__progname__', '__author__', '__copyright__',
+			'__credits__', '__license__', '__version__',
+			'__maintainer__', '__email__', '__status__', 
+			'__date__', '__contact__']
+
+__progname__ = 'Programs Name'
+__author__ = "First name, ..."
+__copyright__ = "Copyright 20xx, The ProjectName Project"
+__credits__ = ["First name USER"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "First name and Last Name"
+__email__ = "email@domain.ext"
+__status__ = "Production"
+__date__ = 'dd.mm.yyyy'
+__contact__ = 'vk, ok, whatsapp, viber ...'
