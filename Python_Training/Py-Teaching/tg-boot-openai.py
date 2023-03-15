1e31cead8d8fcce5b59a4bb518d61a4f89748178
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2023-03-15 09:57:04 +0300

    Update

--- Python_Training/Py-Teaching/tg-boot-openai.py
+++ Python_Training/Py-Teaching/tg-boot-openai.py
@@ -10,16 +10,16 @@ bot = telebot.TeleBot('boot_api_key')
 
 @bot.message_handler(func=lambda _: True)
 def handle_message(message):
-	response = openai.Completion.create(
-				model="text-davinci-003",
-				prompt=message.text,
-				temperature=0.8,
-				max_tokens=1024,
-				top_p=1,
-				frequency_penality=0.5,
-				presence_penality=0.0,
+	completion = openai.Completion.create(
+		engine="text-davinci-003",
+		prompt=message.text,
+		max_tokens=3096,
+		temperature=0.5,
+		top_p=1,
+		frequency_penalty=0,
+		presence_penalty=0
 	)
-	bot.send_message(chat_id=message.from_user.id, text=response["choices"][0]["text"])
+	bot.send_message(chat_id=message.from_user.id, text=completion["choices"][0]["text"])
 
 def main():
 	bot.polling()
