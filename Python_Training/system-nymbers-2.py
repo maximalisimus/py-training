76b55dc5b9253bb68763b8a1d1f576084de955c5
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2025-06-06 23:01:10 +0300

    Update

---
+++ Python_Training/system-nymbers-2.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def convert_to_base(num, base):
+	""" Перевод целых чисел из десятичной системы в заданную. """
+	if num < 0:
+		return '-' + convert_to_base(-num, base)
+	elif num == 0:
+		return '0'
+
+	digits = []
+	while num:
+		digits.append(int(num % base))
+		num //= base
+
+	return ''.join(str(x) for x in digits[::-1])
+
+def convert_float_to_base(num, base, precision=5):
+	""" Перевод дробных чисел из десятичной системы в заданную. """
+	integer_part = int(num)
+	fractional_part = num - integer_part
+
+	integer_result = convert_to_base(integer_part, base)
+
+	fractional_result = []
+	while precision:
+		fractional_part *= base
+		digit = int(fractional_part)
+		fractional_result.append(str(digit))
+		fractional_part -= digit
+		precision -= 1
+
+	return f"{integer_result}.{''.join(fractional_result)}"
+
+def convert_to_decimal(num_str, base):
+	"""Перевод строки числа из заданной системы счисления в десятичную."""
+	return int(num_str, base)
+
+def convert_float_to_decimal(num_str, base):
+	"""Перевод строки числа с плавающей точкой из заданной системы счисления в десятичную."""
+	if '.' in num_str:
+		integer_part, fractional_part = num_str.split('.')
+	else:
+		integer_part, fractional_part = num_str, '0'
+
+	# Перевод целой части
+	integer_decimal = int(integer_part, base)
+
+	# Перевод дробной части
+	fractional_decimal = 0
+	for i, digit in enumerate(fractional_part):
+		fractional_decimal += int(digit, base) / (base ** (i + 1))
+
+	return integer_decimal + fractional_decimal
+
+def convert_integer_to_hex(num):
+	"""Перевод целой части числа в шестнадцатеричную систему."""
+	if num < 0:
+		return '-' + convert_integer_to_hex(-num)
+	elif num == 0:
+		return '0'
+	
+	hex_digits = []
+	while num:
+		remainder = num % 16
+		if remainder < 10:
+			hex_digits.append(str(remainder))
+		else:
+			hex_digits.append(chr(remainder - 10 + ord('A')))  # Преобразование в символы A-F
+		num //= 16
+	
+	return ''.join(hex_digits[::-1])
+
+def convert_fraction_to_hex(fraction, precision=5):
+	"""Перевод дробной части числа в шестнадцатеричную систему."""
+	hex_fraction = []
+	while precision:
+		fraction *= 16
+		digit = int(fraction)
+		if digit < 10:
+			hex_fraction.append(str(digit))
+		else:
+			hex_fraction.append(chr(digit - 10 + ord('A')))
+		fraction -= digit
+		precision -= 1
+
+	return ''.join(hex_fraction)
+
+def convert_float_to_hex(num):
+	"""Перевод числа с плавающей точкой из десятичной системы в шестнадцатеричную."""
+	if '.' in str(num):
+		integer_part, fractional_part = str(num).split('.')
+		integer_part = int(integer_part)
+		fractional_part = float('0.' + fractional_part)
+	else:
+		integer_part = int(num)
+		fractional_part = 0.0
+
+	hex_integer = convert_integer_to_hex(integer_part)
+	hex_fraction = convert_fraction_to_hex(fractional_part)
+
+	return f"{hex_integer}.{hex_fraction}"
+
+def hex_to_decimal(hex_str):
+	"""Перевод строки числа с плавающей точкой из шестнадцатеричной системы в десятичную."""
+	if '.' in hex_str:
+		integer_part, fractional_part = hex_str.split('.')
+	else:
+		integer_part, fractional_part = hex_str, '0'
+
+	# Перевод целой части
+	integer_decimal = int(integer_part, 16)
+
+	# Перевод дробной части
+	fractional_decimal = 0
+	for i, digit in enumerate(fractional_part):
+		fractional_decimal += int(digit, 16) / (16 ** (i + 1))
+
+	return integer_decimal + fractional_decimal
+	
+def add_int_base(num1_str, num2_str, base):
+	"""Сложение двух целых чисел в заданной системе счисления."""
+	num1_decimal = convert_to_decimal(num1_str, base)
+	num2_decimal = convert_to_decimal(num2_str, base)
+	result_decimal = num1_decimal + num2_decimal
+	return convert_to_base(result_decimal, base)
+
+def subtract_int_base(num1_str, num2_str, base):
+	"""Вычитание двух целых чисел в заданной системе счисления."""
+	num1_decimal = convert_to_decimal(num1_str, base)
+	num2_decimal = convert_to_decimal(num2_str, base)
+	result_decimal = num1_decimal - num2_decimal
+	return convert_to_base(result_decimal, base)
+
+def division_int_base(num1_str, num2_str, base):
+	"""Деление двух целых чисел в заданной системе счисления."""
+	num1_decimal = convert_to_decimal(num1_str, base)
+	num2_decimal = convert_to_decimal(num2_str, base)
+	result_decimal = num1_decimal / num2_decimal
+	return convert_to_base(result_decimal, base)
+
+def multiplication_int_base(num1_str, num2_str, base):
+	"""Деление двух целых чисел в заданной системе счисления."""
+	num1_decimal = convert_to_decimal(num1_str, base)
+	num2_decimal = convert_to_decimal(num2_str, base)
+	result_decimal = num1_decimal * num2_decimal
+	return convert_to_base(result_decimal, base)
+
+def add_float_base(num1_str, num2_str, base):
+	"""Сложение двух дробных чисел в заданной системе счисления."""
+	num1_decimal = convert_float_to_decimal(num1_str, base)
+	num2_decimal = convert_float_to_decimal(num2_str, base)
+	result_decimal = num1_decimal + num2_decimal
+	return convert_float_to_base(result_decimal, base)
+
+def subtract_float_base(num1_str, num2_str, base):
+	"""Вычитание двух дробных чисел в заданной системе счисления."""
+	num1_decimal = convert_float_to_decimal(num1_str, base)
+	num2_decimal = convert_float_to_decimal(num2_str, base)
+	result_decimal = num1_decimal - num2_decimal
+	return convert_float_to_base(result_decimal, base)
+
+def division_float_base(num1_str, num2_str, base):
+	"""Деление двух дробных чисел в заданной системе счисления."""
+	num1_decimal = convert_float_to_decimal(num1_str, base)
+	num2_decimal = convert_float_to_decimal(num2_str, base)
+	result_decimal = num1_decimal / num2_decimal
+	return convert_float_to_base(result_decimal, base)
+
+def multiplication_float_base(num1_str, num2_str, base):
+	"""Деление двух дробных чисел в заданной системе счисления."""
+	num1_decimal = convert_float_to_decimal(num1_str, base)
+	num2_decimal = convert_float_to_decimal(num2_str, base)
+	result_decimal = num1_decimal * num2_decimal
+	return convert_float_to_base(result_decimal, base)
+
+def main():
+	print()
+	print(f"Задание 1.") # Задание 1.
+	print(f"a'10 = 219; a'2 = {convert_to_base(219, 2)}") # a'10 = 219; a'2 = 11011011
+	print(f"a'10 = 122; a'4 = {convert_to_base(219, 4)} ") # a'10 = 122; a'4 = 3123 
+	print(f"a'10 = 231; a'8 = {convert_to_base(231, 8)} ") # a'10 = 231; a'8 = 347
+	print(f"a'10 = 213; a'16 = {convert_integer_to_hex(213)} ") # a'10 = 213; a'16 = D5
+	print()
+	print(f"Задание 2.") # Задание 2.
+	print(f"a'10 = 167; a'2 = {convert_to_base(167, 2)}") # a'10 = 167; a'2 = 10100111
+	print(f"a'10 = 167; a'4 = {convert_to_base(167, 4)}") # a'10 = 167; a'4 = 2213
+	print(f"a'10 = 167; a'8 = {convert_to_base(167, 8)}") # a'10 = 167; a'8 = 247
+	print(f"a'10 = 167; a'16 = {convert_integer_to_hex(167)}") # a'10 = 167; a'16 = A7
+	print()
+	print(f"Задание 5.") # Задание 5.
+	print(f"11011011'2 + 10111010'2 = {add_int_base('11011011', '10111010', 2)}") # 11011011'2 + 10111010'2 = 110010101
+	print(f"11011011'2 - 10111010'2 = {subtract_int_base('11011011', '10111010', 2)}") # 11011011'2 - 10111010'2 = 100001
+	print(f"11011011'2 * 10111010'2 = {multiplication_int_base('11011011', '10111010', 2)}") # 11011011'2 * 10111010'2 = 1001111100011110
+	print(f"11011011'2 / 10111010'2 = {division_float_base('11011011', '10111010', 2)}") # 11011011'2 / 10111010'2 = 1.00101
+
+if __name__ == '__main__':
+	main()
